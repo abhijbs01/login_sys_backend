fc80022cc30e69dc6058d47909724a11ae9fd0ff
abhijbs01 <abhishek.jbs01@gmail.com>
2024-05-01 17:34:04 +0530

    add rnv

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const UserRouter = require("./Routes/User");
 const MongoConnect = require("./Config/Db");
+const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const cors = require("cors");
 
 MongoConnect();
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
