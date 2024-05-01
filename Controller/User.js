87cec0450dae59587e681955c235933fe2fc3b66
abhijbs01 <abhishek.jbs01@gmail.com>
2024-05-01 17:13:52 +0530

    add rnv

--- Controller/User.js
+++ Controller/User.js
@@ -86,7 +86,7 @@ async function ForgotPassword(req, res) {
       from: "abhishekvaghasiya00@gmail.com",
       to: email,
       subject: "Reset Password",
-      text: `http://localhost:3000/reset-password/${token}`,
+      text: `https://login-system-dun-gamma.vercel.app/reset-password/${token}`,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "login-system-dun-gamma.vercel.app",
     credentials: true,
 }));
 
