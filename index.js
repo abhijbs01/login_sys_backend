0079619c8e7f3a3d544c66c78e77d13ef3b11942
abhijbs01 <abhishek.jbs01@gmail.com>
2024-05-02 18:59:58 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(cookieParser());
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://login-system-dun-gamma.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 
 app.use("/", UserRouter);
 
